579cdca1338929346b4b826fbfb2dd3103e1cba0
lujialei <lujialei08@163.com>
2018-09-29 09:42:25 +0800

    commit

---
+++ app/src/main/java/luyuan/com/exhibition/ChatFragment.java
@@ -0,0 +1,43 @@
+package luyuan.com.exhibition;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * @author: lujialei
+ * @date: 2018/9/27
+ * @describe:
+ */
+
+
+public class ChatFragment extends Fragment {
+
+    Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
---
+++ app/src/main/java/luyuan/com/exhibition/MainActivity.java
@@ -0,0 +1,21 @@
+package luyuan.com.exhibition;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_view)
+    BottomNavigationView bottomView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        bottomView.initFragment(getSupportFragmentManager());
+    }
+}
